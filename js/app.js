664e1d1634e2240026f58d92c65089fc452dc28f
Quyen Tran <t.quyen1997@yahoo.com>
2020-05-03 19:45:56 -0400

    Add image popup

--- js/app.js
+++ js/app.js
@@ -18,4 +18,15 @@ $(document).ready(function(){
      }
 
  })
+ //Magnific popup
+ $('#projects__modal').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      // options for gallery
+      enabled: true
+    }
+    // other options
+  });
+
 })
